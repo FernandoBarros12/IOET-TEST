9d15cf97047ee8b46189be50313f3d07a09f30aa
Fernando Barros <jbarros.1202@gmail.com>
2022-04-16 17:02:02 -0500

    Dic structure

--- src/estructurado.py
+++ src/estructurado.py
@@ -1,20 +1,18 @@
-'''Test'''
-
+'''Functions to data analyst'''
 
 schedule = []
-with open('Schedule.txt', 'r', encoding='utf-8') as file:
-    schedule=file.readlines()
-d_general = {}  # {name:d_schedule}
+with open(file='./data/Schedule.txt', mode='r', encoding='utf-8') as file:
+    schedule = file.readlines()
+d_general = {}
 
-for employee in schedule:  # creation of d_general
-    info = employee.strip().split("=")
-    week_days = info[1].split(",")
-    d_schedule = {}  # {day:hours}
-    for day in week_days:
-        d_schedule[day[:2]] = day[2:]
-    d_general[info[0]] = d_schedule
-file.close()
+for employee_line in schedule:
+    info = employee_line.strip().split("=")
+    name_employee = info[0]
+    week_days_worked = info[1].split(",")
+    for day_hours_info in week_days_worked:
+        day = day_hours_info[:2]
+        hours_worked = day_hours_info[2:]
+        d_general[day] = d_general.get(day, {})
+        d_general[day][name_employee] = hours_worked
+
 print(d_general)
-
-# for name, dic in d_general.items():
-#     print(dic)
