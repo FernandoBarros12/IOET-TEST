3418c52b9cd0d5ec1829ea90d794894ea27a9ec4
Fernando Barros <jbarros.1202@gmail.com>
2022-04-16 17:35:10 -0500

    Hours format

--- src/estructurado.py
+++ src/estructurado.py
@@ -1,9 +1,22 @@
 '''Functions to data analyst'''
 
+from datetime import time
+
+
+def hour_format(str_hour):
+    '''
+    Return:
+    '''
+    l_hour = str_hour.split(":")
+    return time(int(l_hour[0]), int(l_hour[1]))
+
+
 schedule = []
+d_general = {}
+
+
 with open(file='./data/Schedule.txt', mode='r', encoding='utf-8') as file:
     schedule = file.readlines()
-d_general = {}
 
 for employee_line in schedule:
     info = employee_line.strip().split("=")
@@ -15,4 +28,10 @@ for employee_line in schedule:
         d_general[day] = d_general.get(day, {})
         d_general[day][name_employee] = hours_worked
 
-print(d_general)
+# print(d_general)
+
+for d_user in d_general.values():
+    for name_employee, hours in d_user.items():
+        l_hours = hours.split("-")
+        start_h = hour_format(l_hours[0])
+        end_h = hour_format(l_hours[1])
